abfe8c4a931aff3b4bd2cec6387ca78c0ea97a2f
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-02 11:02:17 +0100

    modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,36 @@
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
-const closeBtn = document.getElementById("closeBtn")
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("myModal");
+  const btn = document.getElementById("myBtn");
+  const closeBtn = document.getElementById("closeBtn");
 
+  btn.onclick = () => {
+    modal.style.display = "block";
+  };
 
-btn.onclick = ()=>{
-    modal.style.display="block"
-} 
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  };
 
-closeBtn.onclick = ()=>{
-    modal.style.display= "none";
-}
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
+  document
+    .querySelector("#contactForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
 
+      const data = {
+        nom: document.querySelector("#Nom").value,
+        prenom: document.querySelector("#Prenom").value,
+        email: document.querySelector("#Email").value,
+        subject: document.querySelector("#Sujet").value,
+        message: document.querySelector("#message").value,
+      };
 
-document.addEventListener("DOMContentLoaded", ()=>{
-
-document
-.querySelector("#contactForm")
-.addEventListener("submit", async(event)=>{
-event.preventDefault();
-
-const data ={
-nom: document.querySelector("#Nom").value,
-prenom: document.querySelector("#Prenom").value,
-email:document.querySelector("#Email").value,
-subject: document.querySelector("#Sujet").value,
-message: document.querySelector("#message").value,
-};
-
-
-const response = await axios.post("http://localhost/3000/form", data);
-alerte ("Merci de nous avoir ecris!")
-});
-
-
+      const response = await axios.post("http://localhost/3000/form", data);
+      alerte("Merci de nous avoir ecris!");
+    });
 });
